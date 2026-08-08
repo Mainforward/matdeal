ee56f61d7b74e7dbafa90999efb283c513159e93
pereligo <igor.perelygin@electrolux.com>
2026-08-08 17:53:08 +0200

    Add product domain model

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -2,7 +2,7 @@
 from app.db.base import Base
 from app.db.database import engine
 
-from app.models.store import Store
+from app.models.store import Product,Store
 
 def init_db():
     Base.metadata.create_all(bind=engine)
